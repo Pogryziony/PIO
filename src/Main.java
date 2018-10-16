7349a2638c8f373cd7f0a190680e861897f373f4
Pogryziony <Pogryziony98@interia.pl>
2018-10-16 15:29:43 +0200

    Kod z 1szych zajec

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Random;
 
 public class Main {
-
 	public static void main(String[] args) {
-		//Created new java project. Nothing special :)
-	}
+		Random rand = new Random();
 
+		int cube;
+		Player player = new Player();
+
+		int guess;
+		do {
+			cube = rand.nextInt(6) + 1;
+			guess = player.guess();
+
+			if (guess != cube)
+				System.out.println("Wrong! Value of Cube = " + cube + " and Your guess = " + guess);
+			else
+				System.out.println("\nWell done! You're right!");
+
+		} while (guess != cube);
+	}
 }
